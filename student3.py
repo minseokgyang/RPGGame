b04eb1cf7ad8c6b8e19d9fbd5c41e9b2125ba6bc
jsg1224 <chilsung0731@gmail.com>
2025-08-28 21:38:55 +0900

    시작

--- student3.py
+++ student3.py
@@ -1,2 +1,70 @@
 def student3():
     print("안녕하세요. 수강생 3번 입니다.")
+
+
+#-----------------------------------------
+import random
+import time
+
+class Skilldamage:
+    @staticmethod
+    def worrior_skill():
+        return random.randint(1,20)
+
+    @staticmethod
+    def thief_skill():
+        return random.randint(2,15)
+
+    @staticmethod
+    def magician_skill():
+        return random.randint(3,17)
+
+    @staticmethod
+    def gambler_skill():
+        return random.randint(0,50)
+    
+    @staticmethod
+    def monster_skill():
+        return random.randint(3,8)
+
+#------------------------------------------
+Monster_hp = random.randint(50,100)
+Player_hp = 50
+class Battle:
+
+        print('전투가 시작되었습니다!')
+        time.sleep(2)
+        while Monster_hp > 0:
+            print('전사가 횡베기를 사용합니다')
+            time.sleep(2)
+            dmg = Skilldamage.worrior_skill()
+            Monster_hp -= dmg
+            if Monster_hp <= 0:
+                print('몬스터에게 최후의 일격을 날렸습니다!')
+                time.sleep(2)
+                print('몬스터를 처치했습니다!')
+                break
+
+            else:
+                print(f'몬스터에게 횡베기를 사용해 체력이 {Monster_hp}남았습니다!')
+                time.sleep(2)
+                print("몬스터에게 턴이 넘어갑니다.")
+                time.sleep(2)
+                print("몬스터의 강한공격!")
+                dmg2 = Skilldamage.monster_skill()
+                Player_hp -= dmg2
+        
+                if Player_hp <= 0:
+                    print('플레이어가 사망했습니다!')
+                    break
+
+                time.sleep(2)
+                print(f'플레이어의 체력이 {Player_hp}남았습니다!')
+                time.sleep(2)
+                print('당신의 공격차례입니다')
+                time.sleep(2)
+        
+                
+                
+        
+
