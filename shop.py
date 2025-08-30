c4b434a236757afd96c0e8f520265bf9910f3476
minseok <minseokgyang@gmail.com>
2025-08-30 17:02:25 +0900

    가게코드 수정

--- shop.py
+++ shop.py
@@ -1,44 +1,64 @@
-from items import generate_stat_item, generate_exp_item, mana_potion, health_potion
+import random
 
-class shop:
-    def __init__(self):
-        self.stock = [
-            generate_stat_item(),
-            generate_exp_item(),
-            mana_potion(),
-            health_potion()
-        ]
-    
+ASCII_ART = '''⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣻⣿⣻⢕
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢕⢕
+⣿⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣛⡛⠫⣻⡻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣏⢕
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢵⠨⠂⠊⠀⠐⠸⡯⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣹⢕
+⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠗⡜⠠⡤⡀⠠⡤⠄⣇⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢕
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡐⢂⠀⣀⡃⢘⡀⠀⡇⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡹⢕
+⣿⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢜⢌⡥⣛⣃⠮⣱⠍⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢕
+⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⠉⠱⡱⡓⣓⡔⡐⣈⢮⡣⠋⠙⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⢕
+⣿⣿⣹⣻⣿⣿⣿⣿⣿⣿⠁⠀⠀⠠⢠⠈⠋⠗⠷⠿⠺⠉⠀⠠⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣗⢕
+⣿⣿⢻⣿⣿⣿⣿⣿⣿⡟⠀⢀⣀⣀⣹⡁⠀⠀⠀⠀⠀⠀⣀⡔⠉⠀⠀⣼⠿⣿⣿⣿⣿⣿⣿⣿⣿⠻⢕
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡎⠁⠀⠀⠸⢛⠊⠉⠉⠀⠐⠒⠓⠓⣸⡀⡌⡀⢔⣼⣿⣿⣿⣿⣿⣿⣟⣕⢕
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⢀⠀⠈⠙⠤⣤⣀⠀⠀⢧⢤⢤⣤⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⢕
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⣀⣰⣠⣀⣤⣶⣾⣿⡷⠓⠃⠀⠸⡁⠁⠛⣿⣿⣿⣿⣿⣿⣿⣿⣟⢕
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠻⣿⡿⠛⠛⠉⠁⠀⡸⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣗⢕
+⠉⠉⠉⠉⠉⠉⠭⠭⠉⠋⣫⢩⡉⠭⠭⠭⠭⢍⣉⣹⣡⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⠭⠭⠭⢭⡭⢷⢕
+⠉⠙⠉⠙⠒⠘⠐⠂⠁⠁⠀⠀⠀⠀⠀⠈⠀⠀⣿⠀⠀⠹⣶⡆⣄⢀⠀⠄⠀⠠⠂⣿⠐⠐⠒⠒⠒⠀⢕
+⠉⠙⠍⠉⠭⠉⠉⠭⠭⠭⠍⠩⠍⠭⠉⢉⡿⠏⠀⠀⠹⣷⣾⢚⣺⠤⠤⢧⢽⡿⠭⠍⠉⠭⠩⠩⢕⢕⢕
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠺⡂⠀⠀⠈⢿⣷⣵⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⢕
+⣟⠈⣟⠈⠀⠃⣿⢀⣟⢸⡇⠚⡈⢏⢸⠊⡘⢸⡧⢤⣴⠄⠀⠀⠀⠈⠉⠋⠁⠀⣿⠀⢏⠇⢽⡟⠅⡌⢕
+⣿⠸⣿⠸⠀⢁⣿⠸⠈⣸⡇⠄⢑⠄⣽⠨⡀⠸⣇⣀⣿⡀⠀⠀⠀⠀⠀⠀⠀⢸⡏⠀⢈⠰⢸⡇⡁⢃⢕
+⣿⠂⣿⠂⡁⢪⣿⢸⠐⣹⡷⡸⠈⠞⣿⢠⡅⡄⢩⢿⣿⣧⠆⠀⠀⠀⠀⡀⠀⠼⣧⣀⠃⠰⣹⡇⣇⠃⢕
+⢛⣿⢛⣿⡟⠽⠿⢿⠛⣿⠻⢿⠻⠻⡻⣿⡿⠛⡛⡿⢿⣿⣟⢆⢁⡴⠁⠀⠀⠀⠉⣿⣿⣿⡿⠿⣿⠿⢕
+⢿⠥⢿⠥⠴⠥⢬⠴⣾⢥⠤⢧⠥⡤⡭⣾⠱⠑⠀⠊⠂⠹⢿⣷⣾⢶⠀⡀⠊⠁⠀⠈⠻⣦⠽⣦⣆⣔⢕
+⠄⠀⠄⠀⠄⠢⠀⣽⠃⠑⡁⠔⡂⠌⡒⡟⠛⡻⠛⡛⠛⡛⠿⣿⣿⣿⣓⠄⠀⠀⠀⠀⠀⠈⢷⣜⡳⣜⢕
+⠆⢀⠆⢀⠈⠂⣱⢛⢟⠛⠛⢛⠛⡛⢻⣇⣌⣌⣔⣠⣑⣁⣡⣻⣿⣿⣿⣇⡀⠀⠀⠀⠀⠀⠀⠙⢯⠘⢕
+⠴⡦⠴⡦⠥⣴⢣⠀⠄⠁⠉⠀⠢⡠⢸⠟⠉⠙⠙⠙⠩⠈⠐⠼⣿⣿⣿⣿⣷⡤⡂⠀⠀⠀⠀⠀⠀⠙⢕
+⠈⣊⠈⣊⣲⢏⢀⠂⡢⠁⡨⠐⠀⠀⣿⠰⡂⠄⠒⠀⢔⢂⢍⠡⢿⣿⣿⣿⣿⣿⣗⡇⠀⠀⠀⠀⠀⠀'''
+
+class Shop:
+    def __init__(self, inventory, money):
+        self.inventory = inventory
+        self.money = money
+
     def show_items(self):
+        print(ASCII_ART)
+        print("=== 상점 목록 ===")
+        print("1. 랜덤 스탯 티켓 (10골드)")
+        print("2. 경험치 스크롤 (5골드, EXP +0~20)")
+        print("3. 마나물약 (3골드)")
+        print("4. 회복약 (3골드)")
+
+    def visit(self, player):
+        self.show_items()
         try:
-            print("=== 상점 진열 ===")
-            for i, item in enumerate(self.stock, 1):
-                print(f"{i}. {item}")
-            print("0. 나가기")
-        except Exception as e:
-            print(f"[오류] 상점 진열 중 문제가 발생하였습니다.: {e}")
-    
-    def visit_shop(self, player):
-        while True:
-            self.show_items()
-            try:
-                choice = input("구매할 아이템 번호를 입력해주세요. (0 입력시 나가기):")
-                choice = int(choice)
-                if choice == 0:
-                    print("[상점] 상점을 나갑니다.")
-                    break
-                if 1 <= choice <= len(self.stock):
-                    item = self.stock[choice - 1]
-                    if player.money.spend(item.price):
-                        player.inventory.add_item(item)
-                        print(f"[상점]{item.name}구매완료")
-                        if item.effect == "exp":
-                            player.add_exp(item.value)
-                    else:
-                        print("[상점] 골드가 부족합니다.")
-                else:
-                    print("[상점] 잘못된 선택입니다.")
-            except ValueError:
-                print("[상점] 잘못된 입력입니다. 숫자를 입력해주세요.")
-            except Exception as e:
-                print("[상점 오류]알 수 없는 문제가 발생했습니다.: {e}")
+            choice = int(input("구매할 아이템 번호를 입력하세요: "))
+            if choice == 1:
+                if self.money.spend(10):
+                    self.inventory.add_item("랜덤스탯티켓")
+            elif choice == 2:
+                if self.money.spend(5):
+                    exp = random.randint(0, 20)
+                    player.add_exp(exp)
+            elif choice == 3:
+                if self.money.spend(3):
+                    self.inventory.add_item("마나물약")
+            elif choice == 4:
+                if self.money.spend(3):
+                    self.inventory.add_item("회복약")
+            else:
+                print("[ERROR] 잘못된 선택입니다.")
+        except ValueError:
+            print("[ERROR] 숫자를 입력해주세요.")
